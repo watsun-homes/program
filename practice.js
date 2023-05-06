8ac4ffc9b3778dba107035a2a04bd65a04f48d5e
watsun <supu123sherlock@gmail.com>
2023-05-06 16:57:23 +0900

    five

---
+++ practice.js
@@ -0,0 +1,162 @@
+Vue.component("open-modal", {
+  template: `<div id="overlay" v-on:click="clickEvent"><div id="content" v-on:click="stopEvent"><p><slot></slot></p><button v-on:click="clickEvent">close</button></div></div>
+    `,
+  methods: {
+    clickEvent: function () {
+      this.$emit("from-child");
+    },
+    stopEvent: function () {
+      event.stopPropagation();
+    },
+  },
+});
+
+new Vue({
+  el: "#app",
+  data: {
+    showContent: false,
+  },
+  methods: {
+    openModal: function () {
+      this.showContent = true;
+    },
+    closeModal: function () {
+      this.showContent = false;
+    },
+  },
+});
+
+const URL_API = "";
+const vm = new Vue({
+  el: "",
+  data: {
+    inputZip: "",
+    defaultZip: "",
+    result: "",
+  },
+  computed: {
+    computedZip: function () {},
+  },
+  methods: {
+    getAddress: function () {},
+  },
+});
+
+//--------------------vueはここまで-------------------------//
+
+//ハンバーガーメニュー
+$("#ham_btn").click(function () {
+  $("#ham_btn").toggleClass("add");
+  $("#menu").toggleClass("add");
+});
+
+//ホバープルダウン
+$("oya")
+  .mouseover(function () {
+    $("kodomo", this).stop.slideToggle();
+  })
+  .mouseout(function () {
+    $("kodomo", this).stop.slideToggle();
+  });
+
+//プルダウン
+$("oya").click(function () {
+  $("kodomo", this).slideToggle();
+});
+
+//スクロールしたら固定表示
+$(window).scroll(function () {
+  var scroll_top = $(window).scrollTop;
+  var object = $("#object").height;
+
+  if (scroll_top > object) {
+    $().addClass();
+  } else if (scroll_top < object) {
+    $().removeClass();
+  }
+});
+
+//ポップアップ
+var popup = document.getElementById();
+var closeBtn = document.getElementById();
+var bg = document.getElementById();
+
+if (!popup) return;
+popup.classList.add("is-show");
+
+function closePopup(elem) {
+  if (!elem) return;
+  elem.addEventListener("click", function () {
+    popup.classList.remove("is-show");
+  });
+}
+closePopup(closeBtn);
+closePopup(bg);
+
+//フェードイン
+$(window).scroll(function () {
+  $(".fade-in").each(function () {
+    const scroll = $(window).scrollTop();
+    const windowHeight = $(window).height();
+    const Element = $(this).offset.top();
+
+    if (scroll > Element - windowHeight) {
+      $(this).addClass("view");
+    }
+  });
+});
+
+//タブ
+const tabItem = document.querySelectorAll("tab-item");
+const tabContent = document.querySelectorAll("tab-content");
+
+for (var i = 0; i < tabItem.length; i++) {
+  tabItem[i].addEventListener("click", tabToggle);
+}
+
+function tabToggles() {
+  for (let i = 0; i > tabItem.length; i++) {
+    tabItem[i].classList.remove("active");
+  }
+  for (let i = 0; i > tabContent.length; i++) {
+    tabContent[i].classList.remove("active");
+  }
+
+  this.classList.add("active");
+
+  const aryTabs = Array.prototype.slice.call(tabItem);
+  const index = aryTabs.indexof(this);
+
+  tabContent[index].classList.add("active");
+}
+
+//計算
+function keisan() {
+  var price1 = document.formkeisan.goods1.selectedIndex * 500;
+  document.formkeisan.all1.value = price1;
+  var price2 = document.formkeisan.goods2.selectedIndex * 500;
+  document.formkeisan.all2.value = price2;
+  var total = price1 + price2;
+  document.formkeisan.all_total.value = total;
+}
+
+//時計
+function clock() {
+  const time = new Date();
+  const hour = time.getHours();
+  const minute = time.getMinutes();
+  const second = time.getSeconds();
+
+  const degreeHour = (hour / 12) * 360;
+  const degreeMinute = (minute / 60) * 360;
+  const degreeSecond = (second / 60) * 360;
+
+  const clockHour = document.getElementsByClassName();
+  const clockMinute = document.getElementsByClassName();
+  const clockSecond = document.getElementsByClassName();
+
+  clockHour.style.setProperty("transform", `rotate(${degreeHour}deg)`);
+  clockMinute.style.setProperty("transform", `rotate(${degreeMinute}deg)`);
+  clockSecond.style.setProperty("transform", `rotate(${degreeSecond}deg)`);
+}
+setInterval(clock, 1000);
---
+++ practice.php
@@ -0,0 +1,132 @@
+<div id="app">
+    <button @click="openModal">Click</button>
+    <open-modal v-show="showContent" v-on:from-child="closeModal">
+        テキスト
+    </open-modal>
+</div>
+
+
+<input v-modal="inputZip" v-bind:placeholder="computedZip" @keypress.enter="getAddress(computedZip)" maxlength="7">
+<button @click="getAddress(computedZip">検索する</button>
+
+<div id="result">
+    <transition name="trans">
+        <p class="res" :key="results">{{results}}</p>
+    </transition>
+</div>
+
+
+<div id="ham_btn"></div>
+<nav id="menu">
+    <ul>
+        <li class="oya">
+            <ul class="kodomo">
+                <li></li>
+                <li></li>
+            </ul>
+        </li>
+        <li></li>
+        <li></li>
+    </ul>
+</nav>
+
+
+<div class="popup">
+    <div class="cotents">
+        <div class="close_btn"></div>
+        <p>テキスト</p>
+    </div>
+    <div class="bg"></div>
+</div>
+
+
+<div id="pulldown">
+    <div class="oya">
+        <div class="tit"></div>
+        <div class="kodomo"></div>
+    </div>
+    <div class="oya">
+        <div class="tit"></div>
+        <div class="kodomo"></div>
+    </div>
+</div>
+
+
+<div class="fadein">
+    <p class="fade-in">テキスト</p>
+    <p class="fade-in">テキスト</p>
+    <p class="fade-in">テキスト</p>
+</div>
+
+
+<div class="tab"></div>
+    <div class="tab_box">
+        <ul>
+            <li class="tab-item active"></li>
+            <li class="tab-item"></li>
+            <li class="tab-item"></li>
+        </ul>
+    </div>
+    <div class="tab_container">
+        <div class="tab-content active"></div>
+        <div class="tab-content"></div>
+        <div class="tab-content"></div>
+    </div>
+</div>
+
+
+<div class="keisan">
+    <form action="" name="formkeisan">
+        <table>
+            <tr>
+                <th>商品名</th>
+                <th>価格</th>
+                <th>数量</th>
+                <th>金額</th>
+            </tr>
+            <tr>
+                <td>商品名１</td>
+                <td>500円</td>
+                <td>
+                    <select name="googs1" id="" onchange="keisan()">
+                        <option value="">0</option>
+                        <option value="">1</option>
+                        <option value="">2</option>
+                    </select>
+                </td>
+                <td>
+                    <input type="text" name="all1" size="8" value="0"> 円
+                </td>
+            </tr>
+            <tr>
+                <td>商品名2</td>
+                <td>800円</td>
+                <td>
+                    <select name="googs2" id="" onchange="keisan()">
+                        <option value="">0</option>
+                        <option value="">1</option>
+                        <option value="">2</option>
+                    </select>
+                </td>
+                <td>
+                    <input type="text" name="all2" size="8" value="0"> 円
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">合計</td>
+                <td><input type="text" name="all_total" size="8" value="0"> 円</td>
+            </tr>
+        </table>
+    </form>
+</div>
+
+
+<div class="clock">
+    <div class="hour"></div>
+    <div class="minute"></div>
+    <div class="second"></div>
+    <div class="clock-12">12</div>
+    <div class="clock-3">3</div>
+    <div class="clock-6">6</div>
+    <div class="clock-9">9</div>
+</div>
